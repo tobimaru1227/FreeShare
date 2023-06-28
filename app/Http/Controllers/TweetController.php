6935b3ab517742f75ffa3289063ba0da9fe027e8
tobimaru <tobimaru1227@gmail.com>
2023-06-28 10:21:12 +0900

    トップページビューの作成

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tweet;
 
 class TweetController extends Controller
 {
     public function index()
     {
-        $tweets = Tweet::all();
+        $tweets = Tweet::orderBy('created_at', 'desc')->get();
         return view('tweets.index', ['tweets' => $tweets]);
     }
 }
--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -1,1 +1,26 @@
-<h1>トップページ</h1>
+<x-app-layout>
+    
+    <x-slot name=header>Home</x-slot>
+    
+    <div class="tweet">
+        <!-- 投稿データ一覧 -->
+        @foreach ($tweets as $tweet)
+            <div class="tweet-container">
+                <!-- 投稿者の表示 -->
+                <div class="user-name">{{ $tweet->user->name }}</div>
+                <!-- 投稿内容の表示 -->
+                <div class="tweet-content">{{ $tweet->content }}</div>
+                <!-- 画像があれば表示 -->
+                @if ($tweet->image)
+                    <img src="{{ asset('storage/' . $tweet->image) }}" alt="投稿画像" class="tweet-image">
+                @endif
+                <!-- 投稿日の表示 -->
+                <div class="tweet-date">{{ $tweet->created_at }}</div>
+
+                <x-primary-button><img src="{{ asset('images/good_icon.png') }}" alt="いいね"></x-primary-button>
+                <x-primary-button><img src="{{ asset('images/share_icon.png') }}" alt="リツイート"></x-primary-button>
+            </div>
+        @endforeach
+    </div>
+
+</x-app-layout>
